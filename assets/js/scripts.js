c9a3fe5b4d549178e298c4c0b4bc85a7019eeae0
Gabriel Augusto Engelles Morais <a58790@alunos.ipb.pt>
2024-08-28 21:43:39 +0100

    Implementada lógica de clique e alternância de jogadores

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,6 +6,35 @@ let messageContainer = document.querySelector('#message')
 let messageText = document.querySelector('#message p')
 let secondPlayer
 
-// contador de jogadas
+// Contador de jogadas
 let player1 = 0
 let player2 = 0
+
+// Evento de click
+for(let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener("click", function() {
+        
+        let el = checkElemento(player1, player2)
+        
+        if(this.childNodes.length == 0) {
+            let cloneEl = el.cloneNode(true)
+            this.appendChild(cloneEl)
+        
+            if(player1 == player2) {
+                player1++
+            } else {
+                player2++
+            }
+        }
+    })
+}
+
+//Verifica quem é o próximo player
+function checkElemento(player1, player2) {
+    if(player1 == player2) {
+        el = x 
+    } else {
+        el = o
+    }
+    return el
+}
