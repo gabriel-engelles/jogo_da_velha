0211318bfd934803133cfb4f44dd6674193d9358
Gabriel Augusto Engelles Morais <a58790@alunos.ipb.pt>
2024-08-28 21:29:51 +0100

    Definindo contador de jogadas

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,3 +5,7 @@ let buttons = document.querySelectorAll('#buttons-container button')
 let messageContainer = document.querySelector('#message')
 let messageText = document.querySelector('#message p')
 let secondPlayer
+
+// contador de jogadas
+let player1 = 0
+let player2 = 0
